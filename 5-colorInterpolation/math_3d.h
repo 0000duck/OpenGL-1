7f3d98457d6bac8d51eee34a91bcbd35e59fc2cf
Fabrizio Perria <fabrizio.perria@gmail.com>
2016-04-11 11:12:57 -0400

    Signed-off-by: Fabrizio Perria <fabrizio.perria@gmail.com>

--- 5-colorInterpolation/math_3d.h
+++ 5-colorInterpolation/math_3d.h
@@ -25,37 +25,9 @@
 #define ToRadian(x) ((x) * M_PI / 180.0f)
 #define ToDegree(x) ((x) * 180.0f / M_PI)
 
-float RandomFloat();
-
-struct Vector2i
+class Vector3f
 {
-    int x;
-    int y;
-};
-
-struct Vector2f
-{
-    float x;
-    float y;
-
-    Vector2f()
-    {
-    }
-
-    Vector2f(float _x, float _y)
-    {
-        x = _x;
-        y = _y;
-    }
-};
-
-
-struct Vector3f
-{
-    float x;
-    float y;
-    float z;
-
+public:
     Vector3f()
     {
     }
@@ -94,78 +66,30 @@ struct Vector3f
         return *this;
     }
 
-    Vector3f Cross(const Vector3f& v) const;
-
-    Vector3f& Normalize();
-
-    void Rotate(float Angle, const Vector3f& Axis);
-
     void Print() const
     {
         printf("(%.02f, %.02f, %.02f)", x, y, z);
     }
-};
 
+	float getX() const
+	{
+		return x;
+	}
 
-struct Vector4f
-{
+	float getY() const
+	{
+		return y;
+	}
+
+	float getZ() const
+	{
+		return z;
+	}
+
+private:
     float x;
     float y;
     float z;
-    float w;
-
-    Vector4f()
-    {        
-    }
-    
-    Vector4f(float _x, float _y, float _z, float _w)
-    {
-        x = _x;
-        y = _y;
-        z = _z;
-        w = _w;
-    }
-    
-    void Print() const
-    {
-        printf("(%.02f, %.02f, %.02f, %.02f)", x, y, z, w);
-    }       
-};
-
-inline Vector3f operator+(const Vector3f& l, const Vector3f& r)
-{
-    Vector3f Ret(l.x + r.x,
-                 l.y + r.y,
-                 l.z + r.z);
-
-    return Ret;
-}
-
-inline Vector3f operator-(const Vector3f& l, const Vector3f& r)
-{
-    Vector3f Ret(l.x - r.x,
-                 l.y - r.y,
-                 l.z - r.z);
-
-    return Ret;
-}
-
-inline Vector3f operator*(const Vector3f& l, float f)
-{
-    Vector3f Ret(l.x * f,
-                 l.y * f,
-                 l.z * f);
-
-    return Ret;
-}
-
-struct PersProjInfo
-{
-    float FOV;
-    float Width; 
-    float Height;
-    float zNear;
-    float zFar;
 };
 
 class Matrix4f
@@ -174,7 +98,8 @@ public:
     float m[4][4];
 
     Matrix4f()
-    {        
+    {
+		InitIdentity();
     }
     
    
@@ -216,46 +141,12 @@ public:
         return Ret;
     }
     
-    Vector4f operator*(const Vector4f& v) const
-    {
-        Vector4f r;
-        
-        r.x = m[0][0]* v.x + m[0][1]* v.y + m[0][2]* v.z + m[0][3]* v.w;
-        r.y = m[1][0]* v.x + m[1][1]* v.y + m[1][2]* v.z + m[1][3]* v.w;
-        r.z = m[2][0]* v.x + m[2][1]* v.y + m[2][2]* v.z + m[2][3]* v.w;
-        r.w = m[3][0]* v.x + m[3][1]* v.y + m[3][2]* v.z + m[3][3]* v.w;
-        
-        return r;
-    }
-    
     void Print() const
     {
         for (int i = 0 ; i < 4 ; i++) {
             printf("%f %f %f %f\n", m[i][0], m[i][1], m[i][2], m[i][3]);
         }       
     }
-
-    void InitScaleTransform(float ScaleX, float ScaleY, float ScaleZ);
-    void InitRotateTransform(float RotateX, float RotateY, float RotateZ);
-    void InitTranslationTransform(float x, float y, float z);
-    void InitCameraTransform(const Vector3f& Target, const Vector3f& Up);
-    void InitPersProjTransform(const PersProjInfo& p);
 };
-
-
-struct Quaternion
-{
-    float x, y, z, w;
-
-    Quaternion(float _x, float _y, float _z, float _w);
-
-    void Normalize();
-
-    Quaternion Conjugate();  
- };
-
-Quaternion operator*(const Quaternion& l, const Quaternion& r);
-
-Quaternion operator*(const Quaternion& q, const Vector3f& v);
 
 #endif	/* MATH_3D_H */
